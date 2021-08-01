894df342c71ed9296d2e1ebbaec1874de26cd901
Kiarash <kiamhn1998@gmail.com>
2021-08-01 22:46:49 +0430

    - edit feature section

--- src/components/home/FeatureSection.js
+++ src/components/home/FeatureSection.js
@@ -128,8 +128,8 @@ const features = [
     color: "#C51162",
     headline: "لیسانس درایوری",
     frenchHeadline: "Permis de conduire",
-    text: "تعرفه: ۲۰ تا ۲۵ یورو\n زمان تحویل: ۲۴ ساعت",
-    frenchText: "Tarif * : 25€ \n Délai de livraison : 24h",
+    text: "تعرفه: از ۲۰ تا ۲۵ یورو\n زمان تحویل: ۲۴ ساعت",
+    frenchText: "Tarif * : 20 à 25€ \n Délai de livraison : 24h",
     icon: <DescriptionIcon style={{ fontSize: iconSize }} />,
     mdDelay: "200",
     smDelay: "200"
